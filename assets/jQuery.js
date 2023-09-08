e440f6c8a0fb9108999668165868b03057b37a51
Arjun Yadav <95169139+ArjunSYadav@users.noreply.github.com>
2023-09-08 17:30:35 +0530

    Update jQuery.js

--- assets/jQuery.js
+++ assets/jQuery.js
@@ -1,1 +1,32 @@
+$(document).ready(function() {
+    // Function to check and toggle the sticky header
+    function checkStickyHeader() {
+        var header = $("#sticky-header");
+        var content = $("#content");
+        var headerHeight = header.outerHeight();
+        var contentTop = content.offset().top;
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= contentTop - headerHeight) {
+            header.addClass("sticky");
+        } else {
+            header.removeClass("sticky");
+        }
+    }
+
+    // Initial check when the page loads
+    checkStickyHeader();
+
+    // Attach the scroll event listener
+    $(window).scroll(function() {
+        checkStickyHeader();
+    });
+
+
+    $("#menu-toggle").click(function() {
+        $(".menu").toggleClass("active");
+    });
+    
+
+});
 
