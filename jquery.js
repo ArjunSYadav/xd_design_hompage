40c4a47644e4d87203bb6d33a8295606d11b9c46
Arjun Yadav <95169139+ArjunSYadav@users.noreply.github.com>
2023-09-08 13:15:33 +0530

    Update jquery.js
    Sticky effect

---
+++ jquery.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    // Function to check and toggle the sticky header
+    function checkStickyHeader() {
+        var header = $("#sticky-header");
+        var content = $("#content");
+        var headerHeight = header.outerHeight();
+        var contentTop = content.offset().top;
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= contentTop - headerHeight) {
+            header.addClass("sticky");
+        } else {
+            header.removeClass("sticky");
+        }
+    }
+
+    // Initial check when the page loads
+    checkStickyHeader();
+
+    // Attach the scroll event listener
+    $(window).scroll(function() {
+        checkStickyHeader();
+    });
+});
